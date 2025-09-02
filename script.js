5e9de2500b950538b861911f1d271b35af609c2e
Adam Kelly <s00252192@atu.ie>
2025-09-03 00:41:20 +0100

    Added character display

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function getAnimeByName(searchString, showOrMovie){
-    let url = "https://api.jikan.moe/v4/anime?q=";
+    let url = `https://api.jikan.moe/v4/anime?q=${searchString}`;
     try {
-        const response = await fetch(url + searchString);
+        const response = await fetch(url);
         const result = await response.json();
 
         let lowestPopularity = result.data[0].popularity;
@@ -22,8 +22,30 @@ async function getAnimeByName(searchString, showOrMovie){
     }
 }
 
+async function getCharacters(id){
+    let url = `https://api.jikan.moe/v4/anime/${id}/characters`;
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        let characters = [];
+        for (let i = 0; i < result.data.length; i++) {
+            if (result.data[i].role == "Main") {
+                characters.push(result.data[i]);
+            }
+            else {
+                break;
+            }
+        }
+
+        return characters;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 async function displayAnime(){
-    document.getElementById("resultsContainer").innerHTML = "";
+    document.getElementById("animeContainer").innerHTML = "";
 
     let showOrMovie = document.getElementById("showOrMovie").value;
     let searchString = document.getElementById("animeInput").value;
@@ -34,7 +56,7 @@ async function displayAnime(){
 
     if (anime != null){
         const animeCard = document.createElement("div");
-        animeCard.className = "animeCard";
+        animeCard.className = "card";
 
         const animeImage = document.createElement("img");
         animeImage.src = anime.images.jpg.large_image_url;
@@ -58,8 +80,44 @@ async function displayAnime(){
         animeSynopsis.textContent = anime.synopsis;
         animeInformation.appendChild(animeSynopsis);
 
+        const characterButton = document.createElement("button");
+        characterButton.textContent = "See Characters";
+        characterButton.addEventListener("click", () => displayCharacters(anime.mal_id));
+        animeInformation.appendChild(characterButton);
+
         animeCard.appendChild(animeInformation);
-        const animeContainer = document.getElementById("resultsContainer");
+        const animeContainer = document.getElementById("animeContainer");
         animeContainer.appendChild(animeCard);
     }
+}
+
+async function displayCharacters(characterId) {
+    document.getElementById("characterContainer").innerHTML = "";
+
+    const characterHeader = document.createElement("h2");
+    characterHeader.id = "mainCharactersHeader";
+    characterHeader.textContent = "Main Characters";
+    document.getElementById("characterContainer").appendChild(characterHeader);
+
+    let characters = await getCharacters(characterId);
+
+    console.log(characters);
+
+    characters.forEach(character => {
+        const characterCard = document.createElement("div");
+        characterCard.className = "card";
+
+        const characterImage = document.createElement("img");
+        characterImage.src = character.character.images.jpg.image_url;
+        characterCard.appendChild(characterImage);
+
+        const characterInformation = document.createElement("div");
+
+        const characterName = document.createElement("h3");
+        characterName.textContent = character.character.name;
+        characterInformation.appendChild(characterName);
+
+        characterCard.appendChild(characterInformation)
+        document.getElementById("characterContainer").appendChild(characterCard);
+    });
 }
